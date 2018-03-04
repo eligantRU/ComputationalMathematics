e7e8dea5023bc899f14bdfdd8a53e4e4e0937c00
eligantRU <eligant.ru@gmail.com>
2018-03-04 13:24:59 +0300

    Removed extra dependencies

--- main.py
+++ main.py
@@ -25,27 +25,23 @@ def clamp(minimum, x, maximum):
     return max(minimum, min(x, maximum))
 
 
-def cruise_control(proportional_factor, integral_factor=0.1, resist_factor=RESIST_FACTOR, cruise_speed=CRUISE_VELOCITY,
-                   max_force=MAX_FORCE):
-    m = ROBOT_MASS
+def cruise_control(proportional_factor, integral_factor=0.1):
     ind = list(range(MAX_TIME))
-    min_force = -max_force
     t = ind * DELTA_TIME
     error_sum = 0
     v = [START_VELOCITY]
     for i in range(MAX_TIME):
-        err = cruise_speed - v[i]
+        err = CRUISE_VELOCITY - v[i]
         error_sum += err
         force = proportional_factor * err + integral_factor * error_sum * DELTA_TIME
-        force = clamp(min_force, force, max_force)
-        v.append(v[i] + (force - resist_factor * v[i]) * DELTA_TIME / m)
+        force = clamp(-MAX_FORCE, force, MAX_FORCE)
+        v.append(v[i] + (force - RESIST_FACTOR * v[i]) * DELTA_TIME / ROBOT_MASS)
     df_tv = pd.DataFrame(v[:MAX_TIME], index=t, columns=["v"])
     df_tv.index.name = "T"
     return df_tv
 
 
-res = cruise_control(proportional_factor=3, integral_factor=0.2, resist_factor=RESIST_FACTOR,
-                     cruise_speed=CRUISE_VELOCITY, max_force=MAX_FORCE)
+res = cruise_control(proportional_factor=3, integral_factor=0.2)
 
 plt.rcParams["font.fantasy"] = "Arial", "Times New Roman", "Tahoma", "Comic Sans MS", "Courier"
 plt.rcParams["font.family"] = "fantasy"
@@ -64,14 +60,14 @@ plt.ylabel("Velocity, m/s", fontsize=14)
 plt.show()
 
 
-def get_no_error_time(data_frame, cruise_speed, velocity_precision):
+def get_no_error_time(data_frame):
     i = len(data_frame) - 1
-    while (abs(cruise_speed - data_frame.v[i]) < velocity_precision) & (i > 1):
+    while (abs(CRUISE_VELOCITY - data_frame.v[i]) < VELOCITY_PRECISION) & (i > 1):
         i -= 1
     return data_frame.index[i]
 
 
-limit_time = get_no_error_time(res, CRUISE_VELOCITY, VELOCITY_PRECISION)
+limit_time = get_no_error_time(res)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -88,9 +84,8 @@ plt.xlabel("Time, s", fontsize=14)
 plt.ylabel("Velocity, m/s", fontsize=14)
 plt.show()
 
-res1 = cruise_control(proportional_factor=3, integral_factor=0.1, resist_factor=RESIST_FACTOR,
-                      cruise_speed=CRUISE_VELOCITY, max_force=MAX_FORCE)
-limit_time1 = get_no_error_time(res1, CRUISE_VELOCITY, VELOCITY_PRECISION)
+res1 = cruise_control(proportional_factor=3, integral_factor=0.1)
+limit_time1 = get_no_error_time(res1)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
@@ -108,11 +103,11 @@ plt.ylabel("Velocity, m/s", fontsize=14)
 plt.show()
 
 
-def get_convergence_rate(v, cruise_speed, p):
+def get_convergence_rate(v, p):
     vf, pf = [], []
     cc = [1.]
     i = 0
-    while v[i] < (cruise_speed - 0.1):
+    while v[i] < (CRUISE_VELOCITY - 0.1):
         i += 1
         vf.append(v[i])
     numv = len(vf)
@@ -122,7 +117,7 @@ def get_convergence_rate(v, cruise_speed, p):
             pf.append(p[n])
     vf = vf * len(p)
     for i in range(1, len(vf)):
-        cc.append(abs(cruise_speed - vf[i]) / abs(cruise_speed - vf[i-1])**pf[i])
+        cc.append(abs(CRUISE_VELOCITY - vf[i]) / abs(CRUISE_VELOCITY - vf[i-1])**pf[i])
     return pd.DataFrame({
         "istep": pd.Series(istep),
         "cc": pd.Series(cc),
@@ -131,7 +126,7 @@ def get_convergence_rate(v, cruise_speed, p):
 
 
 c = [1., 1.1, 1.2]
-convergence_rate_frame = get_convergence_rate(res1.v, CRUISE_VELOCITY, c)
+convergence_rate_frame = get_convergence_rate(res1.v, c)
 b = int(len(convergence_rate_frame) / len(c))
 
 fig = plt.figure()
@@ -145,5 +140,5 @@ plt.ylabel("cc", fontsize=14)
 plt.show()
 
 order = [1.05]
-convergence_rate_frame = get_convergence_rate(res1.v, CRUISE_VELOCITY, order)
+convergence_rate_frame = get_convergence_rate(res1.v, order)
 print("velocity = ", 1 / convergence_rate_frame.cc.mean())
